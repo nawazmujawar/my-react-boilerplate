86fcfdae669a6ad93204206fc03291a5fd949f31
Nawaz Mujawar <nawazmujawar@hotmail.com>
2022-10-08 16:46:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">My Personal React Boilerplate</h1>
+      <h4 className="sub-heading">Modify as you want</h4>
+    </div>
+  );
+};
+
+export default App;
